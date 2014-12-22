0525fee2b208ed7296462d7c2522bc6880a48f7d
ramusus <ramusus@gmail.com>
2014-12-22 23:31:22 +0700

    workaround import signals error

--- vkontakte_polls/__init__.py
+++ vkontakte_polls/__init__.py
@@ -1,4 +1,9 @@
-from . import signals
+from django.core.exceptions import ImproperlyConfigured
+
+try:
+    from . import signals
+except ImproperlyConfigured:
+    pass
 
 VERSION = (0, 7, 0)
 __version__ = '.'.join(map(str, VERSION))
